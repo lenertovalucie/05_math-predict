40d3b041b63cdbafc4fa80a45fabb56a96d5c1dd
Lucie Lénertová <lenertova.lucie@gmail.com>
2022-07-31 09:45:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# napište předpověď toho, co se stane, až se program spustí (do komentářů pod každý příkaz)
+
+print(6 + 2)
+# 
+print(6 - 2)
+#
+print(6 * 2)
+#
+print(10 / 4)
+#
+num = 20
+# 
+num2 = 5
+#
+num = 10
+# 
+result = num - num2
+#
+print(result)
+#
+print("6 + 2")
+
